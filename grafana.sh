bce02de92e75caa9c1e0b27fd8b813dd4fbc2a29
Mayank sharma <mayank14082003@gmail.com>
2024-07-04 10:45:11 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get install -y apt-transport-https software-properties-common wget
 sudo mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
